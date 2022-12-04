4339dcad768de2f201042764126e324bd1255158
Alexandr Chermashentsev <alexandr.chermashentsev@gmail.com>
2022-12-04 15:59:38 +0500

    Create function PrintArray and start programm

--- Project/Program.cs
+++ Project/Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿void PrintArray(string[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] != null && array[i].Length > 0) // проверка на пустоту элемента массива
+        {
+            if(i == array.Length - 1) 
+                Console.Write($"{array[i]}");
+            else Console.Write($"{array[i]}, ");
+        }
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
+
+Console.WriteLine("Enter the integer number - length of the array");
+int userLength = Convert.ToInt32(Console.ReadLine());
+
+string[] userArray = InitialArray(userLength);
+PrintArray(userArray);
